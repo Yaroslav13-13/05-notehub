363c853e7e020353929ed05b0831022c42e69012
Yaroslav13-13 <frilanserwork13@gmail.com>
2025-09-15 20:12:54 +0300

    05-notehub

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,3 +1,20 @@
+import axios from "axios";
+import Modal from "../components/Modal/Modal";
+import NoteForm from "../components/NoteForm/NoteForm";
+import NoteList from "../components/NoteList/NoteList";
+import NoteList from "../components/NoteList/NoteList";
+import Pagination from "../components/Pagination/Pagination";
+import SearchGox from "../components/SearchGox/SearchGox";
+import axios from "axios";
+
 export default function App() {
-  return <div>Hello</div>;
+  return (
+    <div className={css.app}>
+      <header className={css.toolbar}>
+        {/* Компонент SearchBox */}
+        {/* Пагінація */}
+        {/* Кнопка створення нотатки */}
+      </header>
+    </div>
+  );
 }
