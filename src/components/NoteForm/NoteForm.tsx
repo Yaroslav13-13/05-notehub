4561b77f263c47739644129540da26f1496f2d14
Yaroslav13-13 <frilanserwork13@gmail.com>
2025-09-21 10:07:25 +0300

    My working version

--- src/components/NoteForm/NoteForm.tsx
+++ src/components/NoteForm/NoteForm.tsx
@@ -3,24 +3,30 @@ import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
 import css from "./NoteForm.module.css";
 import type { CreateNotePayload } from "../../services/noteService";
+import type { NoteTag } from "../../types/note";
 
 interface NoteFormProps {
   onCreate: (payload: CreateNotePayload) => Promise<void> | void;
   onCancel: () => void;
 }
 
+const noteTags: NoteTag[] = ["Todo", "Work", "Personal", "Meeting", "Shopping"];
+
 const schema = Yup.object().shape({
   title: Yup.string().required("Введіть заголовок"),
   content: Yup.string().required("Введіть текст"),
+  tag: Yup.string()
+    .oneOf(noteTags, "Оберіть валідний тег")
+    .required("Введіть тег"),
 });
 
 const NoteForm: React.FC<NoteFormProps> = ({ onCreate, onCancel }) => {
   return (
     <Formik
-      initialValues={{ title: "", content: "" }}
+      initialValues={{ title: "", content: "", tag: "Todo" as NoteTag }}
       validationSchema={schema}
       onSubmit={async (values, { resetForm }) => {
-        await onCreate(values);
+        await onCreate(values as CreateNotePayload);
         resetForm();
         onCancel();
       }}
@@ -41,6 +47,18 @@ const NoteForm: React.FC<NoteFormProps> = ({ onCreate, onCancel }) => {
               component="div"
               className={css.error}
             />
+          </label>
+
+          <label>
+            Тег
+            <Field as="select" name="tag" className={css.input}>
+              {noteTags.map((tag) => (
+                <option key={tag} value={tag}>
+                  {tag}
+                </option>
+              ))}
+            </Field>
+            <ErrorMessage name="tag" component="div" className={css.error} />
           </label>
 
           <div className={css.actions}>
